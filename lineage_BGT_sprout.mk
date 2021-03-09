6e93831ecd99d24ac410db81308acb48c80d5ee7
Alessandro Astone <ales.astone@gmail.com>
2021-03-09 18:15:41 +0100

    BGT_sprout: Set Bootanimation resolution

--- lineage_BGT_sprout.mk
+++ lineage_BGT_sprout.mk
@@ -14,6 +14,10 @@ $(call inherit-product, device/hmd/BGT_sprout/device.mk)
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2400
+TARGET_SCREEN_WIDTH := 1080
+
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_BGT_sprout
 PRODUCT_DEVICE := BGT_sprout
