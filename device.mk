6d929201ee766e386e794dcf7b336922b98afef1
TH779 <i@779.moe>
2021-08-24 13:10:33 +0800

    BGT_sprout: Import excluded-input-devices.xml from 00WW_3_380_SP05
    Signed-off-by: TH779 <i@779.moe>
    Change-Id: Ie0f8d23f832385e5853bd1cac653759d5acdeec2
    Signed-off-by: Edward Wu <bluehome.wu@gmail.com>

--- device.mk
+++ device.mk
@@ -372,6 +372,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.vibrator.service.hmd_BGT_sprout
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/vibrator/etc/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
+
 # WiFi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
